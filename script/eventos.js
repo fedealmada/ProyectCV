ee337da041258fdf95a5651a31f16240b924f2f8
fedealmada <federico.almada1998@gmail.com>
2023-02-28 21:50:19 -0300

    first commit

---
+++ script/eventos.js
@@ -0,0 +1,53 @@
+function enAmarillo1() {
+Element =document.getElementById("pers");
+Element.src="img/personal2.png";
+}
+function enNegro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/personal.png";
+}
+
+function enAmarillo2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral2.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+
+function enAmarillo3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades2.png";
+}
+function enNegro3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function enAmarillo4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios2.png";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
+
+function enAmarillo5() {
+    Element =document.getElementById("gi");
+    Element.src="img/github2.png";
+}
+function enNegro5() {
+    Element =document.getElementById("gi");
+    Element.src="img/github.png";
+}
+
+function enAmarillo6() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin2.png";
+}
+function enNegro6() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin.png";
+}
